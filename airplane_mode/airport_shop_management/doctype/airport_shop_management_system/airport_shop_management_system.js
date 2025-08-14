95f90e77cac860125c473ae0d20f0acf0a54acf7
Akash Nerella <95671837+akashnerellaa@users.noreply.github.com>
2025-08-14 20:29:04 +0530

    Update airport_shop_management_system.js

--- airplane_mode/airport_shop_management/doctype/airport_shop_management_system/airport_shop_management_system.js
+++ airplane_mode/airport_shop_management/doctype/airport_shop_management_system/airport_shop_management_system.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, Mohammed Anas and contributors
+// Copyright (c) 2024, Akash Nerella  and contributors
 // For license information, please see license.txt
 
 // frappe.ui.form.on("AIRPORT SHOP MANAGEMENT SYSTEM", {
